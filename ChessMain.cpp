b06ef2e90ec5caee212f81680eda3cc7b1c2865f
HoldenCaulfieldICL <ad6813@imperial.ac.uk>
2013-12-06 19:40:20 +0000

    main: many test cases, all commented

--- ChessMain.cpp
+++ ChessMain.cpp
@@ -77,5 +77,61 @@ int main() {
     cb.submitMove("D3", "G6");
     cout << endl;
 
+
+
+    cb.resetBoard();
+    cout << endl;
+
+    cb.submitMove("B1","C3");
+    cb.submitMove("B8","C6");
+    cb.submitMove("D2","D3");
+    cb.submitMove("G8","F6");
+    cb.submitMove("C1","G5");
+    cb.submitMove("F6","G4");
+    cb.submitMove("E2","E3");
+    cb.submitMove("G4","E3");
+    cb.submitMove("F2","E3");
+    cb.submitMove("B7","B5");
+    cb.submitMove("D1","H5");
+    cb.submitMove("G7","G6");
+    cb.submitMove("H5","H4");
+    cb.submitMove("H7","H6");
+    cb.submitMove("G5","H6");
+    cb.submitMove("H8","H6");
+    cb.submitMove("H4","H6");
+    cb.submitMove("F8","H6");
+    cb.submitMove("G1","F3");
+    cb.submitMove("H6","E3");
+    cb.submitMove("H2","H4");
+    cb.submitMove("G6","G5");
+    cb.submitMove("H4","G5");
+    cb.submitMove("A8","B8");
+    cb.submitMove("H1","H8");
+    cout << "ends with black in checkmate?" << endl;
+
+    cb.resetBoard();
+    cout << endl;
+
+
+
+
+
+
+    cb.resetBoard();
+    cout << endl;
+
+
+
+
+
+    cb.resetBoard();
+    cout << endl;
+
+
+
+
+    cb.resetBoard();
+    cout << endl;
+
     return 0;
 }
